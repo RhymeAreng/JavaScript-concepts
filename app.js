99bbc0aa9828a2cd65bf73ae0717b156413bc1b2
RhymeAreng <rrareng@gmail.com>
2026-08-19 01:05:12 +0600

    Initial Commit - Learning JS concepts

---
+++ app.js
@@ -0,0 +1,7 @@
+// Template Literal - uses back tick for print - similar to Fstring in Python
+let pencilPrice = 20;
+let penPrice = 40;
+
+let totalPrice = `The total price is : ${penPrice + pencilPrice} BDT`;
+console.log(totalPrice);
+
